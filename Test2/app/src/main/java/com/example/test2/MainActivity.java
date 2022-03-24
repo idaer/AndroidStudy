52f7203508b057022064e1021a8673e5d14d6c38
idear <1246883544@qq.com>
2022-03-24 17:37:00 +0800

    新建test2项目，实现对按下按钮的获取（OnClickListener

---
+++ Test2/app/src/main/java/com/example/test2/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String mytag="tag";
+
+    Button btn1;
+    Button btn2;
+
+    ConstraintLayout cons2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two_button);
+
+        btn1=findViewById(R.id.btn1);
+        cons2=findViewById(R.id.layout)；
+
+        btn2=new Button(this);
+        btn2.setText("按钮2");
+
+        btn2.setTag("2");
+        btn1.setTag("1");
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+
+        cons2.addView(btn2);
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Button btn=(Button)view;
+
+        if (btn.getTag().equals("2")) {
+            Log.e(mytag,"已按下按钮"+btn.getTag());
+            btn.setVisibility(View.INVISIBLE);
+        }
+        if (btn.getTag().equals("1")) {
+            Log.e(mytag,"已按下按钮"+btn.getTag());
+            btn2.setVisibility(View.VISIBLE);
+        }
+
+    }
+}
